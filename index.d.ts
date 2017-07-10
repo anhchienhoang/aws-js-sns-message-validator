160169c35df0c55ea501262ff66dbaeb4966b354
Chien Hoang <anhchienhoang@gmail.com>
2017-07-10 17:03:18 +0200

    Add message interface

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,13 @@
+export interface ISnSMessage {
+    Message: any;
+    MessageId: string;
+    Type: string;
+    SubscribeURL: string;
+}
+
 class MessageValidator {
     constructor(hostPattern?: string, encoding?: string);
-    validate(hash: string, cb: (error:Error, message: Object) => void);
+    validate(hash: string, cb: (error: Error, message: ISnSMessage) => void);
 }
 
 export default MessageValidator;
