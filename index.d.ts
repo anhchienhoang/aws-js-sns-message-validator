cc56b53e7f9675c2a1bcd7d9eeb7aa18676711db
Chien Hoang <anhchienhoang@gmail.com>
2017-07-10 16:47:25 +0200

    Export default class

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 class MessageValidator {
     constructor(hostPattern?: string, encoding?: string);
-
     validate(hash: string, cb: (error:Error, message: Object) => void);
 }
-export = MessageValidator;
+
+export default MessageValidator;
